299c1207babcbe6a84ee2986a1cb1ab2f49c6256
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-11-06 12:57:38 +0100

    Pass time to `EdgeCost` for initial edges in Dijkstra (#5677)

--- src/thor/dijkstras.cc
+++ src/thor/dijkstras.cc
@@ -336,14 +336,15 @@ void Dijkstras::Compute(std::vector<valhalla::Location>& locations,
 
   // Prepare for a graph traversal
   Initialize(bdedgelabels_, adjacencylist_, costing_->UnitSize());
-  if (expansion_direction == ExpansionType::forward) {
-    SetOriginLocations(graphreader, locations, costing_);
-  } else {
-    SetDestinationLocations(graphreader, locations, costing_);
-  }
 
   // Get the time information for all the origin locations
   auto time_infos = SetTime(locations, graphreader);
+
+  if (expansion_direction == ExpansionType::forward) {
+    SetOriginLocations(graphreader, locations, time_infos, costing_);
+  } else {
+    SetDestinationLocations(graphreader, locations, time_infos, costing_);
+  }
 
   // Compute the isotile
   auto cb_decision = ExpansionRecommendation::continue_expansion;
@@ -772,15 +773,16 @@ void Dijkstras::ComputeMultiModal(
 // Add edge(s) at each origin to the adjacency list
 void Dijkstras::SetOriginLocations(GraphReader& graphreader,
                                    std::vector<valhalla::Location>& locations,
+                                   const std::vector<baldr::TimeInfo>& time_infos,
                                    const cost_ptr_t& costing) {
   // Bail if you want to do a multipath expansion with more locations than edge label/status supports
   if (multipath_ && locations.size() > baldr::kMaxMultiPathId)
     throw std::runtime_error("Max number of locations exceeded");
 
   // Add edges for each location to the adjacency list
-  uint8_t path_id = -1;
+  uint8_t loc_idx = -1;
   for (auto& location : locations) {
-    ++path_id;
+    ++loc_idx;
 
     // Only skip inbound edges if we have other options
     bool has_other_edges = false;
@@ -788,6 +790,8 @@ void Dijkstras::SetOriginLocations(GraphReader& graphreader,
                   [&has_other_edges](const valhalla::PathEdge& e) {
                     has_other_edges = has_other_edges || !e.end_node();
                   });
+
+    const auto& time_info = time_infos[loc_idx];
 
     // Iterate through edges and add to adjacency list
     for (const auto& edge : (location.correlation().edges())) {
@@ -818,7 +822,7 @@ void Dijkstras::SetOriginLocations(GraphReader& graphreader,
 
       // Get cost
       uint8_t flow_sources;
-      Cost cost = costing->EdgeCost(directededge, tile, TimeInfo::invalid(), flow_sources) *
+      Cost cost = costing->EdgeCost(directededge, tile, time_info, flow_sources) *
                   (1.0f - edge.percent_along());
       // Get path distance
       auto path_dist = directededge->length() * (1 - edge.percent_along());
@@ -838,7 +842,7 @@ void Dijkstras::SetOriginLocations(GraphReader& graphreader,
       bdedgelabels_.emplace_back(kInvalidLabel, edgeid, opp_edge_id, directededge, cost, mode_,
                                  Cost{}, path_dist, false, !(costing_->IsClosed(directededge, tile)),
                                  static_cast<bool>(flow_sources & kDefaultFlowMask),
-                                 InternalTurn::kNoTurn, kInvalidRestriction, multipath_ ? path_id : 0,
+                                 InternalTurn::kNoTurn, kInvalidRestriction, multipath_ ? loc_idx : 0,
                                  directededge->destonly() ||
                                      (costing_->is_hgv() && directededge->destonly_hgv()),
                                  directededge->forwardaccess() & kTruckAccess,
@@ -848,7 +852,7 @@ void Dijkstras::SetOriginLocations(GraphReader& graphreader,
 
       // Add EdgeLabel to the adjacency list
       adjacencylist_.add(idx);
-      edgestatus_.Set(edgeid, EdgeSet::kTemporary, idx, tile, multipath_ ? path_id : 0);
+      edgestatus_.Set(edgeid, EdgeSet::kTemporary, idx, tile, multipath_ ? loc_idx : 0);
     }
   }
 }
@@ -857,15 +861,18 @@ void Dijkstras::SetOriginLocations(GraphReader& graphreader,
 void Dijkstras::SetDestinationLocations(
     GraphReader& graphreader,
     std::vector<valhalla::Location>& locations,
+    const std::vector<baldr::TimeInfo>& time_infos,
     const cost_ptr_t& costing) {
   // Bail if you want to do a multipath expansion with more locations than edge label/status supports
   if (multipath_ && locations.size() > baldr::kMaxMultiPathId)
     throw std::runtime_error("Max number of locations exceeded");
 
   // Add edges for each location to the adjacency list
-  uint8_t path_id = -1;
+  uint8_t loc_idx = -1;
   for (auto& location : locations) {
-    ++path_id;
+    ++loc_idx;
+
+    const auto& time_info = time_infos[loc_idx];
 
     // Only skip outbound edges if we have other options
     bool has_other_edges = false;
@@ -905,8 +912,8 @@ void Dijkstras::SetDestinationLocations(
 
       // Get the cost
       uint8_t flow_sources;
-      Cost cost = costing->EdgeCost(directededge, tile, TimeInfo::invalid(), flow_sources) *
-                  edge.percent_along();
+      Cost cost =
+          costing->EdgeCost(directededge, tile, time_info, flow_sources) * edge.percent_along();
       // Get the path distance
       auto path_dist = directededge->length() * edge.percent_along();
 
@@ -937,13 +944,13 @@ void Dijkstras::SetDestinationLocations(
       bdedgelabels_.emplace_back(kInvalidLabel, opp_edge_id, edgeid, opp_dir_edge, cost, mode_,
                                  Cost{}, path_dist, false, !(costing_->IsClosed(directededge, tile)),
                                  static_cast<bool>(flow_sources & kDefaultFlowMask),
-                                 InternalTurn::kNoTurn, restriction_idx, multipath_ ? path_id : 0,
+                                 InternalTurn::kNoTurn, restriction_idx, multipath_ ? loc_idx : 0,
                                  directededge->destonly() ||
                                      (costing_->is_hgv() && directededge->destonly_hgv()),
                                  directededge->forwardaccess() & kTruckAccess,
                                  destonly_restriction_mask);
       adjacencylist_.add(idx);
-      edgestatus_.Set(opp_edge_id, EdgeSet::kTemporary, idx, opp_tile, multipath_ ? path_id : 0);
+      edgestatus_.Set(opp_edge_id, EdgeSet::kTemporary, idx, opp_tile, multipath_ ? loc_idx : 0);
     }
   }
 }
